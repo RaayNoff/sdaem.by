df34de5fe0b4155f0683634b9a484b361a96cba3
RaayNoff <kraxtv471365@gmail.com>
2022-11-04 14:25:35 +0700

    Added font optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.scss';
+import { Inter } from '@next/font/google';
 import type { AppProps } from 'next/app';
 
+const inter = Inter();
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
