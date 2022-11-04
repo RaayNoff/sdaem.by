71138804ad3e7b1ad4515eaa8e2a6d3165218e32
RaayNoff <kraxtv471365@gmail.com>
2022-11-04 20:22:37 +0700

    Added NavLink component

---
+++ components/ui/Navlink.component.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { FC, useRef } from "react";
+import s from "../../styles/components/ui/Navlink.module.scss";
+import { useActiveLinkRef } from "../../hooks/useActiveLinkRef";
+import SVGSelector, { localSVG } from "../../tools/svgSelector";
+
+interface INavlinkProps {
+  href: string;
+  text: string;
+  marker: boolean;
+}
+
+const Navlink: FC<INavlinkProps> = ({ href, marker, text }) => {
+  const linkRef = useActiveLinkRef(href, s.active);
+
+  return (
+    <Link href={href} className={s.navlink} ref={linkRef}>
+      {marker && SVGSelector(localSVG.MARKER_BLACK)}
+      <span className={s.navlink__text}>{text}</span>
+    </Link>
+  );
+};
+
+export default Navlink;
