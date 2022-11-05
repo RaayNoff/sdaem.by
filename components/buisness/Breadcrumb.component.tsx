ce01ed2df3660d487b08f71f06b3d28050b7bdf9
RaayNoff <kraxtv471365@gmail.com>
2022-11-05 20:41:57 +0700

    Added breadcrumb ui component

---
+++ components/buisness/Breadcrumb.component.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import s from "../../styles/components/buisness/Breadcrumb.module.scss";
+import SVGSelector, { localSVG } from "../../tools/SVGSelector";
+
+interface IBreadcrumbProps {
+  className?: string;
+}
+
+const Breadcrumb: FC<IBreadcrumbProps> = ({ className }) => {
+  return (
+    <section className={`${s.breadcrumb} ${className}`}>
+      {SVGSelector(localSVG.BREADCRUMB_HOME)}
+      {SVGSelector(localSVG.BREADCUMB_DOT)}
+      <span className={s.breadcrumb__default}>Новости</span>
+    </section>
+  );
+};
+
+export default Breadcrumb;
